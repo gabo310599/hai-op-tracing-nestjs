37332e0c5dcbc2120ea5cdff9779a3c713188692
Gabriel Ojeda <92034051+gabo310599@users.noreply.github.com>
2023-07-19 16:17:51 -0400

    Update auth.module.ts

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -19,7 +19,7 @@ import { JWT_SECRET } from '../common/config/constants';
       inject: [ConfigService],
       useFactory: (config: ConfigService) => ({
         secret: config.get<string>(JWT_SECRET),
-        signOptions: { expiresIn: '1m'}
+        signOptions: { expiresIn: '30m'}
       }),
     }),
     UserModule
